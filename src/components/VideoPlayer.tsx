1e634c007acd0af7a5bb6267057dffd3fb32be18
Nicklas Hansson <nicke_h90@hotmail.com>
2023-11-18 22:45:35 +0100

    Add simple video player

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 
-export default function VideoPlayer() {
-  return <div>VideoPlayer</div>;
-}
+const VideoPlayer = ({ currentVideo }) => {
+  const videoRef = useRef(null);
+  const [videoUrl, setVideoUrl] = useState<string>("");
+
+  useEffect(() => {
+    if (currentVideo?.sources?.length > 0) {
+      setVideoUrl(currentVideo.sources[0]);
+      if (videoRef.current) {
+        videoRef.current.load();
+      }
+    }
+  }, [currentVideo]);
+
+  if (!currentVideo || videoUrl.length === 0) {
+    return <div>No video selected</div>;
+  }
+
+  return (
+    <div className="video-player">
+      <video ref={videoRef} width="100%" controls={true}>
+        <source src={videoUrl} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default VideoPlayer;
